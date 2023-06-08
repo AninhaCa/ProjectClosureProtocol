6b7dc938d1173fe11fca88875bef888ac7f74e45
Ana Paula Silva de Sousa <aninhapss2000@gmail.com>
2023-06-08 19:09:52 -0300

    Added Project ClosureProtocol

---
+++ ClosureProtocol/ClosureProtocol/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClosureProtocol
+//
+//  Created by Ana Paula Silva de Sousa on 08/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
