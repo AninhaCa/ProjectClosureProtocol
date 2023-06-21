0b23ef4ebbb778e9679c8000ad55559d3df3f102
Ana Paula Silva de Sousa <aninhapss2000@gmail.com>
2023-06-20 21:40:34 -0300

    Added pods

---
+++ ClosureProtocol/Pods/Target Support Files/Pods-ClosureProtocol-ClosureProtocolUITests/Pods-ClosureProtocol-ClosureProtocolUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ClosureProtocol_ClosureProtocolUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ClosureProtocol_ClosureProtocolUITests
+@end
