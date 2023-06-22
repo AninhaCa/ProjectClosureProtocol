88f6fd8239598078cbc0749a0630e84b54010554
Ana Paula Silva de Sousa <aninhapss2000@gmail.com>
2023-06-21 21:49:08 -0300

    Added Xib

---
+++ ClosureProtocol/ClosureProtocol/MyXibCell.swift
@@ -0,0 +1,23 @@
+//
+//  MyXibCell.swift
+//  ClosureProtocol
+//
+//  Created by Ana Paula Silva de Sousa on 21/06/23.
+//
+
+import UIKit
+
+class MyXibCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
